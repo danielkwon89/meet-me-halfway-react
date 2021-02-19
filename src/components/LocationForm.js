3aaf2828248ff2d4f61a87692e8e9583f2ea0d36
danielkwon89 <danielkwon89@gmail.com>
2021-02-19 14:14:06 -0500

    Added geolocation functionality but needs debugging. Refer to comments.

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -22,12 +22,48 @@ export default class LocationForm extends Component {
         console.log(this.state)
     }
 
+    getLocation = (event) => {
+        // stopPropagation is not working as it's still triggering handleSubmit
+        event.stopPropagation()
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.getCoordinates, this.showError);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+
+    getCoordinates = (position) => {
+        console.log(position.coords.latitude, position.coords.longitude)
+    }
+
+    showError = error => {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.")
+            break;
+          case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.")
+            break;
+          case error.TIMEOUT:
+            alert("The request to get user location timed out.")
+            break;
+          case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.")
+            break;
+        }
+    }
+      
+
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit} >
+                <form onSubmit={this.handleSubmit}>
                     <label>Location 1: </label><br />
-                    <input type="text" name="location_1" onChange={this.handleChange} value={this.state.location_1} /><br /><button>Get Current Location</button><br />
+                    <input type="text" name="location_1" onChange={this.handleChange} value={this.state.location_1} /><br />
+
+                    {/* Unsure if this.getLocation is functional as browser is blocking geolocation functionality */}
+                    <button onClick={this.getLocation}>Get Current Location</button><br />
+
                     <label>Location 2: </label><br />
                     <input type="text" name="location_2" onChange={this.handleChange} value={this.state.location_2} /><br />
                     <label>Point of Interest: </label><br />
